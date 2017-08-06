f5df7c884bbba4202c3b93c177feff7f6b6e7346
Potion Story <potionstory@gmail.com>
2017-08-06 23:08:19 +0900

    claculator : oper

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,26 +6,85 @@ export default class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            operand: [],
+            isOper: false,
+            operand01: 0,
+            operand02: 0,
             operator: '',
             result: 0
         }
         this.handleCompute = this.handleCompute.bind(this);
+        this.computeResult = this.computeResult.bind(this);
+        this.computeNumber = this.computeNumber.bind(this);
+        this.computeOper = this.computeOper.bind(this);
+        this.computeUtil = this.computeUtil.bind(this);
     }
-    handleCompute(value, attr){
-        console.log(value + ' : ' + attr);
-        if (attr == 'num'){
-            this.setState({
-                operand: value,
-                result: value
-            });
-            console.log(this.state.operand);
+    computeResult(rlt) {
+        this.setState({
+            isOper: false,
+            operand01 : rlt,
+            operand02 : 0,
+            result: rlt
+        });
+    }
+    handleCompute(value, attr) {
+        switch(attr) {
+            case 'num': this.computeNumber(value, attr); break;
+            case 'oper': this.computeOper(value, attr); break;
+            case 'util': this.computeUtil(value, attr); break;
         }
+    }
+    computeOper(value, attr) {
+        if (value != '='){
+            this.state.isOper = true;
+            this.state.operator = value;
+        } else if (this.state.isOper == true){
+            let n1 = parseInt(this.state.operand01);
+            let n2 = parseInt(this.state.operand02);
+            let rlt = 0;
+            switch(this.state.operator) {
+                case '÷': 
+                    rlt = n1 / n2;
+                    this.computeResult(rlt);
+                    break;
+                case '×': 
+                    rlt = n1 * n2;
+                    this.computeResult(rlt);
+                    break;
+                case '−': 
+                    rlt = n1 - n2;
+                    this.computeResult(rlt);
+                    break;
+                case '+': 
+                    rlt = n1 + n2;
+                    this.computeResult(rlt);
+                    break;
+            };
+        }
+    }
+    computeNumber(value, attr) {
+        let n = '';
+        if (!this.state.isOper){
+            n = ((this.state.operand01 == 0) || (this.state.operator != '')) ? value : this.state.operand01 + String(value);
+            this.setState({
+                operand01: n,
+                operator: '',
+                result: n
+            });
+        } else {
+            n = (this.state.operand02 == 0) ? value : this.state.operand02 + String(value);
+            this.setState({
+                operand02: n,
+                result: n
+            });
+        }
+    }
+    computeUtil(value, attr) {
+        this.state.result = 'fsdfsdfsd';
     }
     render() {
         return(
             <div className="calculator">
-                <Output Onresult={this.state.result}/>
+                <Output onResult={this.state.result}/>
                 <Input onValue={this.handleCompute}/>
             </div>
         );
--- src/components/Output.js
+++ src/components/Output.js
@@ -4,7 +4,7 @@ export default class Output extends React.Component {
     render() {
         return(
             <div className="output">
-                {this.props.Onresult}
+                {this.props.onResult}
             </div>
         );
     }
