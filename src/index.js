d65f2c955970543c5e1827efdbe09272cf5bb640
potionstory <potionstory@gmail.com>
2017-08-16 15:59:09 +0900

    test1

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './components/App';
 import ResetStyle from './styles/reset.scss';
 import CalculatorStyle from './styles/calculator.scss';
 
+alert('fsdfdsfsd');
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App/>, rootElement);
